b333a498b2b19546ef96a50abab51ea0db1d219c
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-10-05 03:38:40 -0500

    creating index.js and adding pseudocode

---
+++ index.js
@@ -0,0 +1,28 @@
+// must use inquirer
+// should use jest for unit tests
+
+// first class = employee parent class
+// employee class will have following properties and methods
+// name
+// id
+// email
+// getName()
+// getId()
+//getEmail()
+//getRole()--returns 'Employee'
+
+// Other 3 classes will extend 'Employee'
+
+// Manager will have 'Employee's' properties and also;
+// officeNumber
+// getRole()--overridden to return 'Manager'
+
+// Engineer will have 'Employee's' as well as;
+// github--GitHub username
+// getGithub()
+// getRole()--overridden to return 'Engineer'
+
+// Inter will have 'Employee's' as well as;
+// school
+// getSchool()
+// getRole()--overridden to return 'Intern'
