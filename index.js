44bd963a44609ac42ae5adcf6488a467faae8b06
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-10-05 03:44:24 -0500

    adding more pseudocode

--- index.js
+++ index.js
@@ -26,3 +26,19 @@
 // school
 // getSchool()
 // getRole()--overridden to return 'Intern'
+
+
+// Need to accept user input
+// Email address will open default email program and populate the "To" field
+// Link GitHub usernames to their profiles
+    // profiles need to open in a new tab
+
+// Create questions to promp user;
+    // team manager's name
+    // employee ID
+    // email address
+    // office number
+
+
+
+// Need to generate index.html with all of the info entered by user
