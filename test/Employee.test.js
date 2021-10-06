b44483816221d35f925c7c9df225b7b9d5336a76
Alfredo Zavaleta <zavaleta@Alfredos-MacBook-Pro.local>
2021-10-06 03:07:12 -0500

    adding name to employee test

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,5 +1,14 @@
+//See act 11
 const Employee = require("../lib/Employee");
 
 describe ("Employee", () => {
-    
-})
+    it("creates an employee object", () => {
+        const newEmployee = new Employee ();
+        expect(typeof(newEmployee)).toBe("object");
+    });
+    it("can create a name", () => {
+        const name = "Manny";
+        const newEmployee = new Employee(name);
+        expect(newEmployee.name).toBe(name);
+    }
+});
