d81376623d8db65e2266e4c42d55e2934ab1dd4e
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-10-06 18:32:44 -0500

    got tests to run

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,14 +1,29 @@
 //See act 11
+
 const Employee = require("../lib/Employee");
 
-describe ("Employee", () => {
-    it("creates an employee object", () => {
-        const newEmployee = new Employee ();
-        expect(typeof(newEmployee)).toBe("object");
-    });
-    it("can create a name", () => {
-        const name = "Manny";
-        const newEmployee = new Employee(name);
-        expect(newEmployee.name).toBe(name);
-    }
+describe("Employee", () => {
+  it("creates an employee object", () => {
+    const newEmployee = new Employee();
+    expect(typeof newEmployee).toEqual("object");
+  });
+//   console.log("Employee created!");
+  it("can create a name", () => {
+    const name = "Manny";
+    const newEmployee = new Employee(name);
+    expect(newEmployee.name).toEqual(name);
+  });
+//   console.log("Name created!");
+  it("can create an id", () => {
+    const testId = 100;
+    const newEmployee = new Employee("foobar", testId);
+    expect(newEmployee.id).toEqual(testId);
+  });
+//   console.log("Id created!");
+  it("can create an email", () => {
+    const testEmail = "mail@email.com";
+    const newEmployee = new Employee("foobar", 101, testEmail);
+    expect(newEmployee.email).toEqual(testEmail);
+  });
+  //console.log("Email created!");
 });
---
+++ test/Manager.test.js
@@ -0,0 +1,10 @@
+// extends from Employee
+// also needs officeNumber and getRole() returns Manager
+
+const { isTypedArray } = require("util/types")
+const Employee = require("../lib/Employee")
+const Manager = require("../lib/Manager")
+
+describe('Manager', () => {
+    it('')
+})
